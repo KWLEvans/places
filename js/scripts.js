c2c9cf46356348c24d29a34c719f37b9b0ec810e
Azure Rose Legacy and Keith Evans <keith@>
2017-01-23 09:50:34 -0800

    add front-end display functionality

--- js/scripts.js
+++ js/scripts.js
@@ -24,5 +24,24 @@ $(function() {
     var notes = $("#notes").val();
 
     var placeObject = new Place(city, country, landmarks, timeOfYear, notes);
+
+    $("#city").val("");
+    $("#country").val("");
+    $("#landmarks").val("");
+    $("#timeOfYear").val("");
+    $("#notes").val("");
+
+    $("#placeInfo").append("<h3>" + placeObject.city + ", " + placeObject.country + "</h3>");
+    $("#placeInfo").append("<ul></ul>");
+    $("#placeInfo ul:last-of-type").append("<li>" + placeObject.landmarks + "</li>");
+    $("#placeInfo ul:last-of-type").append("<li>" + placeObject.timeOfYear + "</li>");
+    $("#placeInfo ul:last-of-type").append("<li>" + placeObject.notes + "</li>");
+
+
+    $("li").hide();
+
+    $("#placeInfo h3:last-of-type").click(function() {
+      $(this).next().children().toggle();
+    });
   });
 });
